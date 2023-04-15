c01140a89d54ed1ad06819ca4618799948bc0d6f
Tim Benniks <tbenniks@gmail.com>
2023-04-15 18:35:17 +0200

    desc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,6 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Alive and Kicking - A Vue into Rock & Roll",
-      description: "This is the Vue guitar Karaoke talk. The audience can vote for a song to be played live on stage. The whole experience is done in the browser and uses Vue and Nuxt.",
 
       htmlAttrs: {
         lang: "en",
@@ -44,6 +43,10 @@ export default defineNuxtConfig({
         {
           property: "og:image",
           content: "https://res.cloudinary.com/dwfcofnrd/image/upload/c_fill,w_1920,h_1080,g_face/f_auto/q_auto/Alive%20and%20Kicking/332022097_722321479543137_5355332749224365662_n.jpg"
+        },
+        {
+          name: "description",
+          content: "This is the Vue guitar Karaoke talk. The audience can vote for a song to be played live on stage. The whole experience is done in the browser and uses Vue and Nuxt."
         }
       ],
       script: [
