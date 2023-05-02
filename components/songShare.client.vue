aa5654743102ad3b4ad0ebe800e0517bee7619a3
Tim Benniks <tbenniks@gmail.com>
2023-05-02 23:20:37 +0200

    better share? YOLO

--- components/songShare.client.vue
+++ components/songShare.client.vue
@@ -6,7 +6,7 @@ const conference = songStore.conference;
 
 const song = ref(props.song?.song);
 const songId = ref(props.song?.songId);
-const artist = ref(props.song?.artist.replace("/", "%2F"));
+const artist = ref(props.song?.artist);
 </script>
 
 <template>
@@ -14,7 +14,10 @@ const artist = ref(props.song?.artist.replace("/", "%2F"));
     target="_blank"
     class="block underline font-black uppercase mt-3"
     :href="`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-      `I just voted for ${song} at by ${artist} @timbenniks talk at ${conference}`
+      `I just voted for ${song} at by ${artist.replace(
+        '/',
+        '%2F'
+      )} @timbenniks talk at ${conference}`
     )}&url=${encodeURI(
       `https://www.aliveandkicking.dev/share/${songId}?userid=${user.id}`
     )}`"
