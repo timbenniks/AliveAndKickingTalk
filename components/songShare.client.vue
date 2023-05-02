54d6fc1ac6b6db3b0fa222c6f1867cf8b772c856
Tim Benniks <tbenniks@gmail.com>
2023-05-02 23:18:10 +0200

    simplified sharing

--- components/songShare.client.vue
+++ components/songShare.client.vue
@@ -7,29 +7,12 @@ const conference = songStore.conference;
 const song = ref(props.song?.song);
 const songId = ref(props.song?.songId);
 const artist = ref(props.song?.artist.replace("/", "%2F"));
-
-async function share() {
-  const shareData = {
-    title: `I just voted for ${song} by ${artist.value} at @timbenniks talk at ${conference}`,
-    url: `https://www.aliveandkicking.dev/share/${songId}?userid=${user.id}`,
-  };
-
-  if (navigator.share) {
-    await navigator.share(shareData);
-  }
-}
 </script>
 
 <template>
-  <button
-    @click="share()"
-    class="md:hidden underline font-black uppercase block"
-  >
-    SHARE MY VOTE
-  </button>
   <a
     target="_blank"
-    class="hidden md:block underline font-black uppercase mt-3"
+    class="block underline font-black uppercase mt-3"
     :href="`https://twitter.com/intent/tweet?text=${encodeURIComponent(
       `I just voted for ${song} at by ${artist} @timbenniks talk at ${conference}`
     )}&url=${encodeURI(
