ba34594a2aaef98c372a7f556b138a93fa0b2f8a
Tim Benniks <tbenniks@gmail.com>
2024-01-14 20:19:43 +0100

    cleaned up Ref imports

--- components/audioplayer-visualizer.client.vue
+++ components/audioplayer-visualizer.client.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import type { Ref } from "vue";
-
 const viz: Ref<HTMLCanvasElement | null> = ref(null);
 const instance = getCurrentInstance();
 
--- components/audioplayer.client.vue
+++ components/audioplayer.client.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import type { Ref } from "vue";
-
 type events =
   | "play"
   | "pause"
