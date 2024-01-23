d5fb65a3d896d41309261f667320df6da50cf606
Tim Benniks <tbenniks@gmail.com>
2024-01-23 16:22:08 +0100

    preliminary stats page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
-      exclude: ['/share/*', '/live', '/live/*', '/band', '/stats'],
+      exclude: ['/share/*', '/live', '/live/*', '/band', '/stats', '/practise'],
     }
   },
 
---
+++ pages/practise.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+const songStore = useSongStore();
+await songStore.getSongs();
+
+const { allSongs } = storeToRefs(songStore);
+</script>
+
+<template>
+  <main class="p-12">
+    <ul>
+      <li v-for="song in allSongs" :key="song.songId">
+        <a :href="`/live/${song.songId}`">{{ song.song }}</a>
+      </li>
+    </ul>
+  </main>
+</template>
