ab982a5639080c2103b123a1555e1b29dbc01edc
Tim Benniks <tbenniks@gmail.com>
2024-01-28 19:50:47 +0100

    better looking share image

--- pages/share/mashup.vue
+++ pages/share/mashup.vue
@@ -59,7 +59,7 @@ const cldOptions = {
       },
       effects: [
         {
-          opacity: 20,
+          opacity: 30,
           width: 960,
           height: 540,
         },
@@ -73,7 +73,7 @@ const cldOptions = {
       },
       effects: [
         {
-          opacity: 20,
+          opacity: 30,
           width: 960,
           height: 540,
         },
@@ -88,7 +88,7 @@ const cldOptions = {
       },
       effects: [
         {
-          opacity: 20,
+          opacity: 30,
           width: 960,
           height: 540,
         },
@@ -103,7 +103,7 @@ const cldOptions = {
       },
       effects: [
         {
-          opacity: 20,
+          opacity: 30,
           width: 960,
           height: 540,
         },
@@ -115,8 +115,8 @@ const cldOptions = {
       publicId: songs && songs[0].cover,
       position: {
         gravity: "north_west",
-        x: 50,
-        y: 50,
+        x: 340,
+        y: 80,
       },
       effects: [
         {
@@ -131,8 +131,8 @@ const cldOptions = {
       publicId: songs && songs[1].cover,
       position: {
         gravity: "north_east",
-        x: 50,
-        y: 50,
+        x: 340,
+        y: 80,
       },
       effects: [
         {
@@ -146,8 +146,8 @@ const cldOptions = {
       publicId: songs && songs[2].cover,
       position: {
         gravity: "south_west",
-        x: 50,
-        y: 240,
+        x: 340,
+        y: 180,
       },
       effects: [
         {
@@ -162,8 +162,8 @@ const cldOptions = {
       publicId: songs && songs[3].cover,
       position: {
         gravity: "south_east",
-        x: 50,
-        y: 240,
+        x: 340,
+        y: 180,
       },
       effects: [
         {
@@ -172,139 +172,6 @@ const cldOptions = {
           height: 300,
         },
       ],
-    },
-
-    // song info
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 40,
-        fontWeight: "black",
-        text: songs && songs[0].artist,
-      },
-      position: {
-        gravity: "north_west",
-        x: 380,
-        y: 50,
-      },
-    },
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 30,
-        fontWeight: "light",
-        text: songs && songs[0].song,
-      },
-      position: {
-        gravity: "north_west",
-        x: 380,
-        y: 90,
-      },
-    },
-
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 40,
-        fontWeight: "black",
-        text: songs && songs[1].artist,
-      },
-      position: {
-        gravity: "north_east",
-        x: 380,
-        y: 50,
-      },
-    },
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 30,
-        fontWeight: "light",
-        text: songs && songs[1].song,
-      },
-      position: {
-        gravity: "north_east",
-        x: 380,
-        y: 90,
-      },
-    },
-
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 40,
-        fontWeight: "black",
-        text: songs && songs[2].artist,
-      },
-      position: {
-        gravity: "south_west",
-        x: 380,
-        y: 516,
-      },
-    },
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 30,
-        fontWeight: "light",
-        text: songs && songs[2].song,
-      },
-      position: {
-        gravity: "south_west",
-        x: 380,
-        y: 480,
-      },
-    },
-
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 40,
-        fontWeight: "black",
-        text: songs && songs[3].artist,
-      },
-      position: {
-        gravity: "south_east",
-        x: 380,
-        y: 516,
-      },
-    },
-    {
-      width: 500,
-      crop: "fit",
-      text: {
-        color: "white",
-        fontFamily: "Lato",
-        fontSize: 30,
-        fontWeight: "light",
-        text: songs && songs[3].song,
-      },
-      position: {
-        gravity: "south_east",
-        x: 380,
-        y: 480,
-      },
     },
 
     // User
@@ -338,6 +205,295 @@ const cldOptions = {
       ],
     },
   ],
+  // overlays: [
+  //   // Art
+  //   {
+  //     publicId: songs && songs[0].art,
+  //     position: {
+  //       gravity: "north_west",
+  //       y: -50,
+  //     },
+  //     effects: [
+  //       {
+  //         opacity: 20,
+  //         width: 960,
+  //         height: 540,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     publicId: songs && songs[1].art,
+  //     position: {
+  //       gravity: "north_east",
+  //       y: -50,
+  //     },
+  //     effects: [
+  //       {
+  //         opacity: 20,
+  //         width: 960,
+  //         height: 540,
+  //       },
+  //     ],
+  //   },
+
+  //   {
+  //     publicId: songs && songs[2].art,
+  //     position: {
+  //       gravity: "south_west",
+  //       y: 50,
+  //     },
+  //     effects: [
+  //       {
+  //         opacity: 20,
+  //         width: 960,
+  //         height: 540,
+  //       },
+  //     ],
+  //   },
+
+  //   {
+  //     publicId: songs && songs[3].art,
+  //     position: {
+  //       gravity: "south_east",
+  //       y: 50,
+  //     },
+  //     effects: [
+  //       {
+  //         opacity: 20,
+  //         width: 960,
+  //         height: 540,
+  //       },
+  //     ],
+  //   },
+
+  //   // Covers
+  //   {
+  //     publicId: songs && songs[0].cover,
+  //     position: {
+  //       gravity: "north_west",
+  //       x: 50,
+  //       y: 50,
+  //     },
+  //     effects: [
+  //       {
+  //         border: "3px_solid_white",
+  //         width: 300,
+  //         height: 300,
+  //       },
+  //     ],
+  //   },
+
+  //   {
+  //     publicId: songs && songs[1].cover,
+  //     position: {
+  //       gravity: "north_east",
+  //       x: 50,
+  //       y: 50,
+  //     },
+  //     effects: [
+  //       {
+  //         border: "3px_solid_white",
+  //         width: 300,
+  //         height: 300,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     publicId: songs && songs[2].cover,
+  //     position: {
+  //       gravity: "south_west",
+  //       x: 50,
+  //       y: 240,
+  //     },
+  //     effects: [
+  //       {
+  //         border: "3px_solid_white",
+  //         width: 300,
+  //         height: 300,
+  //       },
+  //     ],
+  //   },
+
+  //   {
+  //     publicId: songs && songs[3].cover,
+  //     position: {
+  //       gravity: "south_east",
+  //       x: 50,
+  //       y: 240,
+  //     },
+  //     effects: [
+  //       {
+  //         border: "3px_solid_white",
+  //         width: 300,
+  //         height: 300,
+  //       },
+  //     ],
+  //   },
+
+  //   // song info
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 40,
+  //       fontWeight: "black",
+  //       text: songs && songs[0].artist,
+  //     },
+  //     position: {
+  //       gravity: "north_west",
+  //       x: 380,
+  //       y: 50,
+  //     },
+  //   },
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 30,
+  //       fontWeight: "light",
+  //       text: songs && songs[0].song,
+  //     },
+  //     position: {
+  //       gravity: "north_west",
+  //       x: 380,
+  //       y: 90,
+  //     },
+  //   },
+
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 40,
+  //       fontWeight: "black",
+  //       text: songs && songs[1].artist,
+  //     },
+  //     position: {
+  //       gravity: "north_east",
+  //       x: 380,
+  //       y: 50,
+  //     },
+  //   },
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 30,
+  //       fontWeight: "light",
+  //       text: songs && songs[1].song,
+  //     },
+  //     position: {
+  //       gravity: "north_east",
+  //       x: 380,
+  //       y: 90,
+  //     },
+  //   },
+
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 40,
+  //       fontWeight: "black",
+  //       text: songs && songs[2].artist,
+  //     },
+  //     position: {
+  //       gravity: "south_west",
+  //       x: 380,
+  //       y: 516,
+  //     },
+  //   },
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 30,
+  //       fontWeight: "light",
+  //       text: songs && songs[2].song,
+  //     },
+  //     position: {
+  //       gravity: "south_west",
+  //       x: 380,
+  //       y: 480,
+  //     },
+  //   },
+
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 40,
+  //       fontWeight: "black",
+  //       text: songs && songs[3].artist,
+  //     },
+  //     position: {
+  //       gravity: "south_east",
+  //       x: 380,
+  //       y: 516,
+  //     },
+  //   },
+  //   {
+  //     width: 500,
+  //     crop: "fit",
+  //     text: {
+  //       color: "white",
+  //       fontFamily: "Lato",
+  //       fontSize: 30,
+  //       fontWeight: "light",
+  //       text: songs && songs[3].song,
+  //     },
+  //     position: {
+  //       gravity: "south_east",
+  //       x: 380,
+  //       y: 480,
+  //     },
+  //   },
+
+  //   // User
+  //   {
+  //     url: avatar,
+  //     position: {
+  //       gravity: "center",
+  //       y: -50,
+  //     },
+  //     effects: [
+  //       {
+  //         width: 300,
+  //         height: 300,
+  //         radius: 600,
+  //       },
+  //     ],
+  //   },
+
+  //   // Marketing
+  //   {
+  //     publicId: "Alive and Kicking/vuejsams24-overlay-mashup-6",
+  //     position: {
+  //       x: 0,
+  //       y: 0,
+  //     },
+  //     effects: [
+  //       {
+  //         width: 1920,
+  //         height: 1080,
+  //       },
+  //     ],
+  //   },
+  // ],
 };
 
 const url = constructCloudinaryUrl({
